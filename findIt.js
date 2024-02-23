3675dc921758fb3495065c2a77eda3120744fcb1
Kurt.Dubain <kurt.du.cobain@gmail.com>
2024-02-23 14:04:24 +0800

    优化了两套代码
    1、优化了单一处理的输入输出流程
    2、优化了批次处理的提前断开连接的bug
    3、优化了批次处理的结构，取消了模块化

--- findIt.js
+++ findIt.js
@@ -1,60 +1,94 @@
-const csv = require('csv-parser');
-const fs = require('fs');
-// const mysql = require('mysql2/promise');
-
+const csv = require("csv-parser");
+const fs = require("fs");
+//获取用户输入
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const mysql = require("mysql2/promise");
+// 测试
+const dbConfig = {
+  host: "43.143.148.172",
+  port: 3306,
+  user: "neon",
+  password: "NeonOnTestServer",
+  database: "amazon_boot",
+};
+// 正式
 // const dbConfig = {
-//   host: 'localhost',
-//   user: 'yourUsername',
-//   password: 'yourPassword',
-//   database: 'yourDatabase'
+//     host: '127.0.0.1',
+//     port: 3306,
+//     user: 'Amazon',
+//     password: 'Amazon20240221',
+//     database: 'amazon_boot',
+//     socketPath: '/var/run/mysqld/mysqld.sock'
 // };
-
-const csvFilePath = './1.csv';
-// const batchSize = 1000; // 每批1000条记录
+let csvFilePath = "";
+const batchSize = 1000; // 每批1000条记录
 
 async function importCsvData() {
+  console.log("begin", new Date());
   try {
-    // const connection = await mysql.createConnection(dbConfig);
-    // await connection.beginTransaction(); // 开始事务
-    console.log("Connected successfully to MySQL");
+    const connection = await mysql.createConnection(dbConfig);
+    await connection.beginTransaction(); // 开始事务
+    console.log("MySql连接成功");
 
     let batch = [];
     fs.createReadStream(csvFilePath)
       .pipe(csv())
-      .on('data', (row) => {
-        const rank = row['报告范围=["每日"],选择日期=["2024-02-07"]'];
-        // const rank = row['搜索频率排名']
-        const desc = row['_1'];
-        const timestamp = row['_20'];
-        batch.push([rank, desc, timestamp]);
-        console.log(row)
-        // console.log(row)
-        // return
-        // if (batch.length >= batchSize) {
-        //   connection.query(
-        //     'INSERT INTO keyword_list (rank, desc, timestamp) VALUES ?',
-        //     [batch]
-        //   );
-        //   batch = []; // 重置批次
-        // }
+      .on("data", async (row) => {
+        let rank;
+        let desc;
+        let timestamp;
+        let i = 0;
+        // 简单遍历获取对象键值对方便之后处理
+        for (let key in row) {
+          let item = row[key];
+          if (i == 0) {
+            rank = item;
+          } else if (i == 1) {
+            desc = item;
+          } else if (i == 20) {
+            timestamp = item;
+          }
+          i++;
+        }
+        if (desc.includes(keyWord)) {
+          batch.push([rank, desc, timestamp]);
+        }
+        if (batch.length >= batchSize) {
+          connection.query(
+            "INSERT INTO search_words (`rank`, `desc`, `timestamp`) VALUES ?",
+            [batch]
+          );
+          batch = []; // 重置批次
+        }
       })
-      .on('end', async () => {
-        console.log(batch)
-        // if (batch.length > 0) {
+      .on("end", async () => {
+        if (batch.length > 0) {
           // 处理最后一批数据
-        //   await connection.query(
-        //     'INSERT INTO keyword_list (rank, desc, timestamp) VALUES ?',
-        //     [batch]
-        //   );
-        // }
-        // await connection.commit(); // 提交事务
-        // console.log("Data has been successfully inserted into MySQL");
-        // await connection.end();
+          await connection.query(
+            "INSERT INTO search_words (`rank`, `desc`, `timestamp`) VALUES ?",
+            [batch]
+          );
+        }
+        await connection.commit(); // 提交事务
+        console.log("数据已经插入数据库中", new Date());
+        await connection.end();
       });
   } catch (err) {
-    console.error("An error occurred:", err);
-    await connection.rollback(); // 回滚事务
+    console.error("出现错误:", err);
   }
 }
-
-importCsvData();
+let keyWord = "";
+rl.question("请输入你希望查找的关键词：", (answer) => {
+  keyWord = answer;
+  console.log(`关键词是：${answer}！`);
+  rl.question("请输入你希望查找的文件名：", (answer) => {
+    csvFilePath = "../" + answer;
+    console.log(`查找的文件名是：${answer}！`);
+    rl.close();
+    importCsvData();
+  });
+});
--- findThem.js
+++ findThem.js
@@ -1,82 +1,134 @@
-const csv = require('csv-parser');
-const fs = require('fs');
-const readline = require('readline');
-
+const fs = require("fs");
+const csv = require("csv-parser");
+const path = require("path");
+const readline = require("readline");
+// const { importCsvData } = require('./4insert_test.js')
+const mysql = require("mysql2/promise");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
-const mysql = require('mysql2/promise');
-
+const directoryPath = path.join(__dirname, "../"); // 父目录路径
+// // 正式数据库配置
 const dbConfig = {
-  host: '43.143.148.172',
+  host: "127.0.0.1",
   port: 3306,
-  user: 'neon',
-  password: 'NeonOnTestServer',
-  database: 'amazon_boot'
+  user: "Amazon",
+  password: "Amazon20240221",
+  database: "amazon_boot",
+  socketPath: "/var/run/mysqld/mysqld.sock",
 };
-
-const csvFilePath = './1.csv';
-const batchSize = 1000; // 每批1000条记录
-
-async function importCsvData() {
+// 测试
+// const dbConfig = {
+//     host: '43.143.148.172',
+//     port: 3306,
+//     user: 'neon',
+//     password: 'NeonOnTestServer',
+//     database: 'amazon_boot'
+// };
+console.log("开始了~", new Date());
+rl.question("请输入你希望查找的关键词：", async (keyWord) => {
+  console.log(`关键词是：${keyWord}！`);
+  rl.close();
   try {
     const connection = await mysql.createConnection(dbConfig);
-    await connection.beginTransaction(); // 开始事务
     console.log("Connected successfully to MySQL");
 
-    let batch = [];
-    fs.createReadStream(csvFilePath)
-      .pipe(csv())
-      .on('data', (row) => {
-        let rank;
-        let desc;
-        let timestamp;
-        let i = 0
-        for (let key in row) {
-          let item = row[key]
-          if (i == 0) {
-            rank = item
-          } else if (i == 1) {
-            desc = item
-          } else if (i == 20) {
-            timestamp = item
-          }
-          i++
-        }
-        if (desc.includes(keyWord)) {
-          batch.push([rank, desc, timestamp]);
-        }
-        if (batch.length >= batchSize) {
-          connection.query(
-            'INSERT INTO search_words (rank, desc, timestamp) VALUES ?',
-            [batch]
-          );
-          batch = []; // 重置批次
-        }
-      })
-      .on('end', async () => {
-        if (batch.length > 0) {
-          // 处理最后一批数据
-          await connection.query(
-            'INSERT INTO search_words (rank, desc, timestamp) VALUES ?',
-            [batch]
-          );
-          console.log(batch, 'batch');
-        }
-        await connection.commit(); // 提交事务
-        console.log("Data has been successfully inserted into MySQL");
+    fs.readdir(directoryPath, async (err, files) => {
+      if (err) {
+        console.log("Unable to scan directory: " + err);
         await connection.end();
+        return;
+      }
+      let nums = 0;
+      let arrs = files.filter((file) =>
+        file.match(/^US_热门搜索词_简单_Week_20\d{2}_\d{2}_\d{2}\.csv$/)
+      );
+      const promises = arrs.map((file) => {
+        // importCsvData(path.join(directoryPath, file), keyWord, connection)
+
+        let batch = [];
+        let stream = fs
+          .createReadStream(path.join(directoryPath, file))
+          .pipe(csv());
+        stream
+          .on("data", async (row) => {
+            let rank, desc, timestamp;
+            let i = 0;
+            for (let key in row) {
+              let item = row[key];
+              if (i == 0) rank = item;
+              else if (i == 1) desc = item;
+              else if (i == 20) timestamp = item;
+              i++;
+            }
+            if (desc.includes(keyWord)) {
+              batch.push([rank, desc, timestamp]);
+            }
+            if (batch.length >= 1000) {
+              // 注意这里移除了原有的await，因为.on('data')不支持异步函数
+              stream.pause();
+              let res = await connection.query(
+                "INSERT INTO search_words (`rank`, `desc`, `timestamp`) VALUES ?",
+                [batch]
+              );
+              // console.log(res);
+              stream.resume();
+              batch = []; // 重置批次
+            }
+          })
+          .on("end", async () => {
+            if (batch.length > 0) {
+              try {
+                stream.pause();
+                const results = await connection.query(
+                  "INSERT INTO search_words (`rank`, `desc`, `timestamp`) VALUES ?",
+                  [batch]
+                );
+                let [ResultSetHeader] = results;
+                if (ResultSetHeader.affectedRows != 1000) {
+                  nums++;
+                  if (nums == arrs.length) {
+                    await connection.end();
+                  }
+                }
+                // console.log(results)
+                if (ResultSetHeader.affectedRows > 0) {
+                  console.log(
+                    "Data has been successfully inserted for",
+                    path.join(directoryPath)
+                  );
+                } else {
+                  console.log("No rows were inserted.");
+                }
+                stream.resume();
+              } catch (error) {
+                console.log(error);
+              }
+            } else {
+              console.log("No data to insert.");
+              nums++;
+              if (nums == arrs.length) {
+                await connection.end();
+              }
+            }
+          })
+          .on("close", () => {
+            console.log(111);
+          });
       });
+      try {
+        // 使用Promise.all等待所有文件处理完成
+        let res = await Promise.all([...promises]);
+        // if (res.length === arrs.length) {
+        //   let cRes = await connection.end();
+        //   console.log(res, arrs, 'all结束');
+        // }
+      } catch (err) {
+        console.error("An error occurred during file processing:", err);
+      }
+    });
   } catch (err) {
     console.error("An error occurred:", err);
-    // await connection.rollback(); // 回滚事务
   }
-}
-let keyWord = ''
-rl.question('请输入你希望查找的关键词：', (answer) => {
-  keyWord = answer
-  console.log(`关键词是：${answer}！`);
-  rl.close();
-  importCsvData();
 });
